1896ce13a96ce73d4b7dea2447d2532596c9461e
Valentin GOSSET <vgosset@e3r9p9.42.fr>
2016-05-11 15:35:48 +0200

    l

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 18:05:46 by vgosset           #+#    #+#             */
-/*   Updated: 2016/05/10 18:43:59 by vgosset          ###   ########.fr       */
+/*   Updated: 2016/05/11 15:35:25 by vgosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,38 @@ void	parse(void)
 {
 	char	*line;
 	int		check;
+	int		count;
 
+	count = 0;
 	check = 0;
-	line = NULL;
 
-	while (get_next_line(0, &line) = 1)
+	while (get_next_line(0, &line) == 1)
 	{
-		if (into_struct(line) = -1)
-			ft_putndl("error");
-		else
+		if (into_struct(line) == -1)
+		{
 			check = 1;
+			break;
+		}
+		if (into_struct(line) == 0)
+		{
+			check = 2;
+			break;
+		}
+		count++;
 	}
-	if (check = 1)
+	if (check == 0)
 	{
-		while (get_next_line(0, &line) = 1)
-			ft_putndl(line);
+		while (get_next_line(0, &line) == 1)
+			ft_putendl(line);
 	}
+	if (check == 2)
+	{
+		while (get_next_line(0, &line) == 1 && count != 0)
+		{
+			ft_putendl(line);
+			count--;
+		}
+	}
+	else
+		ft_putendl("error");
+}
